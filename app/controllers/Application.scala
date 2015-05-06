b231b4a02dcb8d67c7576d7945ab4f80ed21b34b
Andy Boyle <andyjcboyle@gmail.com>
2015-05-06 13:06:28 +0100

    Ensure only https is used

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,31 +1,54 @@
 package controllers
 
+import play.api.libs.concurrent.Execution.Implicits._
+import play.Logger
 import play.api.mvc._
+
+import scala.concurrent.Future
 
 object Application extends Controller with EmailInterestRegistered with EmailEnquiry {
 
-  def index = Action {
+  def index = SecureAction {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def about = Action {
+  def about = SecureAction {
     Ok(views.html.about())
   }
 
-  def classes = Action {
+  def classes = SecureAction {
     Ok(views.html.classes())
   }
 
-  def contacts = Action {
+  def contacts = SecureAction {
     Ok(views.html.contacts())
   }
 
-  def skills = Action {
+  def skills = SecureAction {
     Ok(views.html.skills())
   }
 
-  def monkeynews = Action {
+  def monkeynews = SecureAction {
     Ok(views.html.monkeynews())
   }
 
 }
+
+object SecureAction extends ActionBuilder[Request] {
+  def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
+    Logger.debug("Calling Secure Action")
+    val secureRequest = new SecureRequest[A](request)
+    val httpsPortVal = System.getProperty("https.port")
+    val httpsPort = if (httpsPortVal != null) httpsPortVal else "443"
+    if ( request.secure ) {
+      block(secureRequest)
+    } else {
+      Future(Results.Redirect(s"https://${request.domain}:" + httpsPort + s"${request.uri}"))
+    }
+  }
+}
+
+class SecureRequest[A](request: Request[A]) extends WrappedRequest[A](request) {
+  override def secure = true
+}
+
