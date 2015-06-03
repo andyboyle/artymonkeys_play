d0d298aedd45561617e85e8073ec7875065ed07a
Andy Boyle <andyjcboyle@gmail.com>
2015-06-03 19:34:52 +0100

    current state

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,52 +2,61 @@ package controllers
 
 import controllers.dao.{NewsHomeDao, CustomerDao, UserDao}
 import controllers.email.{EmailEnquiry, EmailInterestRegistered}
+import play.Routes
 import play.api.mvc._
 
+
 object Application extends Controller
-with EmailInterestRegistered with EmailEnquiry with Secured  {
+with EmailInterestRegistered with EmailEnquiry with Secured {
 
   val usersDao = new UserDao()
   val customersDao = new CustomerDao()
   val newsHomeDao = new NewsHomeDao()
 
   def index = SecureAction { request =>
-    println("Index Success: " + request.cookies.get("gtoken"))
-    Ok(views.html.index(isAdminUser(request),newsHomeDao.getLastNewsItems()))
+    Ok(views.html.index(isAdminUser(request), newsHomeDao.getLastNewsItems()))
   }
 
   def about = SecureAction { request =>
-    println("About Success: " + request.cookies.get("gtoken"))
     Ok(views.html.about(isAdminUser(request)))
   }
 
   def classes = SecureAction { request =>
-    println("Classes Success: " + request.cookies.get("gtoken"))
     Ok(views.html.classes(isAdminUser(request)))
   }
 
   def contacts = SecureAction { request =>
-    println("Contacts Success: " + request.cookies.get("gtoken"))
     Ok(views.html.contacts(isAdminUser(request)))
   }
 
+//  def locationClassTimes(classLocation: String) = SecureAction { request =>
+//    val validClassTimesForLocation = List("Hello1", "Hello2", classLocation)
+//    Ok(views.html.contacts(!isAdminUser(request), validClassTimesForLocation))
+//  }
+
+  def ajaxTest() = SecureAction { request =>
+    val someHtml = "<h1>Howdy doodly doo!<d2>"
+    Ok(someHtml)
+  }
+
+  def locationClassTimes : List[String] =  {
+    List("Hello1", "Hello2")
+  }
+
   def skills = SecureAction { request =>
-    println("Skills Success: " + request.cookies.get("gtoken"))
     Ok(views.html.skills(isAdminUser(request)))
   }
 
   def monkeynews = SecureAction { request =>
-    println("Monkey News Success: " + request.cookies.get("gtoken"))
     Ok(views.html.monkeynews(isAdminUser(request)))
   }
 
   def oauth2call = SecureAction { request =>
-    println("Oauth 2 callback Success: " + request.session)
     Ok(views.html.oauth())
   }
 
   def loginSuccess = SecureAction { request =>
-    if ( isAdminUser(request)) {
+    if (isAdminUser(request)) {
       Ok(views.html.loginsuccess(isAdminUser(request)))
     } else {
       Unauthorized(views.html.unauthorised())
@@ -55,7 +64,6 @@ with EmailInterestRegistered with EmailEnquiry with Secured  {
   }
 
   def logoutSuccess = SecureAction {
-    //    Ok(views.html.logoutsuccess())
     Redirect(routes.Application.index()).withNewSession.flashing(
       "success" -> "You've been logged out"
     )
@@ -90,16 +98,6 @@ with EmailInterestRegistered with EmailEnquiry with Secured  {
       Unauthorized(views.html.unauthorised())
     }
   }
-
-//  def locationClasses = SecureAction { request =>
-//    Ok(Seq("Hello1","Hello2"))
-//    if (isAdminUser(request)) {
-//      Ok(views.html.managenews(true, newsHomeDao.getLastNewsItems()))
-//    } else {
-//      Unauthorized(views.html.unauthorised())
-//    }
-//  }
-
 
   def customers = SecureAction { request =>
     if (isAdminUser(request)) {
