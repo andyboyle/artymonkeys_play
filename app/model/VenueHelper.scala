46168d60877a24a24d7aba8f4bd131f61edeb58a
Andy Boyle <andyjcboyle@gmail.com>
2015-06-20 08:47:14 +0100

    a bit of pixel pushing

--- app/model/VenueHelper.scala
+++ app/model/VenueHelper.scala
@@ -8,7 +8,6 @@ object VenueHelper {
     val times = ApplicationCake.venueService.retrieveVenueClassTimes(venueName)
     val htmlOptions = times.map( x => "<option value='" + x.replaceAll(" ","") + "'>" + x +"</option>" )
 
-    val htmlToReturn =
     "<div id=\"monkeyclasstimesdiv\" class=\"formelement\">\n" +
       "Time:\n" +
       "<select id=\"monkeyclasstimes\" class=\"howdidyouhear\" name=\"time\" required>\n" +
@@ -16,8 +15,6 @@ object VenueHelper {
           htmlOptions +
       "</select>\n" +
     "</div>\n"
-
-    return htmlToReturn
   }
 
 }
